866b725163b6170b5b2c9ea82da428825ea4243a
sgob <marco.sgobino@gmail.com>
2023-11-03 15:52:30 +0100

    Improved update.sh with release management

--- update.sh
+++ update.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 author="Marco Sgobino"
-old_version=27.3.2
+email="<marco.sgobino@gmail.com>"
+old_version=27.3.3
 new_version=27.3.3
+old_release=3
+new_release=4
 
-today=$(date "+%a %b %d %T %Z %Y")
-content="Release v${new_version}"
+today=$(LANG=C date "+%a %b %d %T %Z %Y")
+content="- Fixed formatting of descriptions"
+content+="\n"
+content+="- Introduced new changelog format"
 
-entry="* ${today} ${author} - v${new_version}"
+entry="* ${today} ${author} ${email} - v${new_version}-${new_release}"
 entry+="\n"
-entry+="- ${content}"
+entry+="${content}"
 
 find . -type f -name '*.spec' -exec sed -i          \
     -e "/Version:/s/${old_version}/${new_version}/" \
+    -e "/Release:/s/${old_release}/${new_release}/" \
     -e "/%changelog/a ${entry}" {} \;
